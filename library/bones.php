a417bbe222da2ae8815dec88a6af9286e13641cd
WPsites <simon@wpsites.co.uk>
2012-09-13 17:01:10 +0200

    Add paths to .LESS style sheets for easier enabling of LESS

--- library/bones.php
+++ library/bones.php
@@ -118,9 +118,11 @@ function bones_scripts_and_styles() {
  
     // register main stylesheet
     wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), '', 'all' );
+    //less: wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/less/style.less', array(), '', 'all' );
 
     // ie-only style sheet
     wp_register_style( 'bones-ie-only', get_stylesheet_directory_uri() . '/library/css/ie.css', array(), '' );
+    //less: wp_register_style( 'bones-ie-only', get_stylesheet_directory_uri() . '/library/less/ie.less', array(), '' );
     
     // comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
